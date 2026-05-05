7a6649130aa90d0128602f8e62e016b5f4f147a6
Vasia <vgrican48@gmail.com>
2026-05-05 15:38:15 +0300

    Написання коду до методу перебору та проходження тестів

--- lab_3/script.js
+++ lab_3/script.js
@@ -1,23 +1,45 @@
 class KnapsackSolver {
-    constructor(W, items) {
-        this.W = W;
-        this.items = items;
-        this.n = items.length;
+  constructor(W, items) {
+    this.W = W;
+    this.items = items;
+    this.n = items.length;
+  }
+  // Метод перебору
+  solveBruteForce() {
+    let maxVal = 0;
+    const totalCombinations = 1 << this.n;
+    for (let i = 0; i < totalCombinations; i++) {
+      let currentWeight = 0;
+      let currentValue = 0;
+      for (let j = 0; j < this.n; j++) {
+        if (i & (1 << j)) {
+          currentWeight += this.items[j].weight;
+          currentValue += this.items[j].value;
+        }
+      }
+      if (currentWeight <= this.W && currentValue > maxVal) {
+        maxVal = currentValue;
+      }
     }
+    return { maxValue: maxVal };
+  }
 }
-
 
 const W_test = 24;
 const items_test = [
-    { weight: 9, value: 11 }, { weight: 3, value: 10 },
-    { weight: 3, value: 6 },  { weight: 3, value: 4 },
-    { weight: 5, value: 15 }, { weight: 8, value: 3 },
-    { weight: 9, value: 12 }
+  { weight: 9, value: 11 },
+  { weight: 3, value: 10 },
+  { weight: 3, value: 6 },
+  { weight: 3, value: 4 },
+  { weight: 5, value: 15 },
+  { weight: 8, value: 3 },
+  { weight: 9, value: 12 },
 ];
 
 function runTests() {
-    console.log("ЗАПУСК ТЕСТІВ");
-    const solver = new KnapsackSolver(W_test, items_test);
-    
+  console.log("ЗАПУСК ТЕСТІВ");
+  const solver = new KnapsackSolver(W_test, items_test);
+  console.assert(solver.solveBruteForce().maxValue === 47, "Помилка: Перебір");
+  console.log("Метод 1 (Перебір) - тест пройдено. Очікувано: 47");
 }
 runTests();
