61c113054e65c03f4b833264290b4e66abd12930
Vasia <vgrican48@gmail.com>
2026-05-05 17:45:07 +0300

    Реалізація методу динамічного програмування та методу гілок

--- lab_3/script.js
+++ lab_3/script.js
@@ -53,6 +53,46 @@ class KnapsackSolver {
     }
     return { maxValue: currentValue };
   }
+
+  // Метод динамічного програмування
+  solveDP() {
+    let dp = Array(this.n + 1)
+      .fill()
+      .map(() => Array(this.W + 1).fill(0));
+    for (let i = 1; i <= this.n; i++) {
+      for (let w = 1; w <= this.W; w++) {
+        if (this.items[i - 1].weight <= w) {
+          dp[i][w] = Math.max(
+            dp[i - 1][w],
+            dp[i - 1][w - this.items[i - 1].weight] + this.items[i - 1].value,
+          );
+        } else {
+          dp[i][w] = dp[i - 1][w];
+        }
+      }
+    }
+    return { maxValue: dp[this.n][this.W], dpTable: dp };
+  }
+
+  // Метод гілок
+  solveBranchAndBound() {
+    let memo = new Map();
+    const bnb = (w, i) => {
+      if (i === this.n || w === 0) return 0;
+      const key = `${w}-${i}`;
+      if (memo.has(key)) return memo.get(key);
+      let res =
+        this.items[i].weight > w
+          ? bnb(w, i + 1)
+          : Math.max(
+              bnb(w, i + 1),
+              this.items[i].value + bnb(w - this.items[i].weight, i + 1),
+            );
+      memo.set(key, res);
+      return res;
+    };
+    return { maxValue: bnb(this.W, 0) };
+  }
 }
 
 const W_test = 24;
@@ -77,5 +117,14 @@ function runTests() {
   console.log(
     `Метод 3 (Жадібний) - завершено. Знайдено: ${greedyRes}. (Цей метод може не співпадати з 47)`,
   );
+  console.assert(
+    solver.solveDP().maxValue === 47,
+    "Помилка: Динамічне програмування",
+  );
+  console.assert(
+    solver.solveBranchAndBound().maxValue === 47,
+    "Помилка: Гілок",
+  );
+  console.log("Метод 4 та Метод 5 - тести пройдено.");
 }
 runTests();
